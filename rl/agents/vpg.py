c63ec08ea4f9e8aa1fb1a5cffffd52da804aa956
Akshay Kurmi <akskurmi@gmail.com>
2021-03-29 17:38:07 -0400

    Improve performance of VPG

--- rl/agents/vpg.py
+++ rl/agents/vpg.py
@@ -15,7 +15,6 @@ class VPG:
             buffer_size=replay_buffer_size,
             store_fields=[
                 ReplayField('observation', shape=self.env.observation_space.shape),
-                ReplayField('observation_next', shape=self.env.observation_space.shape),
                 ReplayField('action', dtype=np.int32),
                 ReplayField('reward'),
                 ReplayField('done', dtype=np.bool),
@@ -37,13 +36,12 @@ class VPG:
         observation = previous_transition['observation_next'] if previous_transition else self.env.reset()
         action = self.policy.sample(tf.expand_dims(observation, axis=0)).numpy()[0]
         observation_next, reward, done, _ = self.env.step(action)
-        transition = {'observation': observation, 'observation_next': observation_next,
-                      'action': action, 'reward': reward, 'done': done}
+        transition = {'observation': observation, 'action': action, 'reward': reward, 'done': done}
         if training:
             self.replay_buffer.store_transition(transition)
             for m in self.metrics:
                 m.record(transition)
-        return transition
+        return {'observation_next': observation_next, **transition}
 
     def update(self):
         dataset = self.replay_buffer.as_dataset(num_batches=1, batch_size=None)
