bb35786ec82a462ad8bc0db176bc5f7dfbc826a0
Akshay Kurmi <akskurmi@gmail.com>
2021-03-29 17:25:20 -0400

    Refactor VPG to use upgraded replay buffers and training loops

--- rl/agents/vpg.py
+++ rl/agents/vpg.py
@@ -1,78 +1,69 @@
+import numpy as np
 import tensorflow as tf
-from tqdm import tqdm
 
-from rl.replay_buffer import ReplayBuffer
+from rl.metrics import AverageReturn, AverageEpisodeLength
+from rl.replay_buffer import DeterministicReplayBuffer, ReplayField
 
 
 class VPG:
-    def __init__(self, env, policy_fn, lr, epochs, episodes_per_epoch, max_episode_length, ckpt_epochs, log_epochs,
-                 ckpt_dir, log_dir):
+    def __init__(self, env, policy_fn, lr, replay_buffer_size, metrics_buffer_size):
         self.env = env
         self.policy = policy_fn(env.observation_space.shape, env.action_space.n)
         self.lr = lr
-        self.epochs = epochs
-        self.episodes_per_epoch = episodes_per_epoch
-        self.max_episode_length = max_episode_length
-        self.ckpt_epochs = ckpt_epochs
-        self.log_epochs = log_epochs
-        self.ckpt_dir = ckpt_dir
-        self.log_dir = log_dir
 
-        self.replay_buffer = ReplayBuffer()
-        self.epochs_done = tf.Variable(0, dtype=tf.int64, trainable=False)
+        self.replay_buffer = DeterministicReplayBuffer(
+            buffer_size=replay_buffer_size,
+            store_fields=[
+                ReplayField('observation', shape=self.env.observation_space.shape),
+                ReplayField('observation_next', shape=self.env.observation_space.shape),
+                ReplayField('action', dtype=np.int32),
+                ReplayField('reward'),
+                ReplayField('done', dtype=np.bool),
+            ],
+            compute_fields=[
+                ReplayField('episode_return'),
+            ],
+        )
+        self.metrics = [
+            AverageReturn(metrics_buffer_size),
+            AverageEpisodeLength(metrics_buffer_size),
+        ]
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.lr)
-        self.ckpt = tf.train.Checkpoint(policy=self.policy, optimizer=self.optimizer, epochs_done=self.epochs_done)
-        self.ckpt_manager = tf.train.CheckpointManager(self.ckpt, self.ckpt_dir, max_to_keep=1)
-        self.ckpt.restore(self.ckpt_manager.latest_checkpoint).expect_partial()
 
-    def train(self):
-        summary_writer = tf.summary.create_file_writer(self.log_dir)
-        with tqdm(total=self.epochs, desc='Training', unit='epoch') as pbar:
-            pbar.update(self.ckpt.epochs_done.numpy())
-            while self.ckpt.epochs_done.numpy() <= self.epochs:
-                self.replay_buffer.purge()
-                for episode in range(self.episodes_per_epoch):
-                    observation = self.env.reset()
-                    for step in range(self.max_episode_length):
-                        action = self.policy.sample(observation.reshape(1, -1)).numpy()[0]
-                        observation_next, reward, done, _ = self.env.step(action)
-                        self.replay_buffer.store_transition(observation, action, reward)
-                        observation = observation_next
-                        if done:
-                            self.replay_buffer.terminate_episode(observation)
-                            break
+    def variables_to_checkpoint(self):
+        return {'policy': self.policy, 'optimizer': self.optimizer}
 
-                data = self.replay_buffer.get(['observations', 'actions', 'total_rewards', 'episode_lengths'])
+    def step(self, previous_transition=None, training=False):
+        observation = previous_transition['observation_next'] if previous_transition else self.env.reset()
+        action = self.policy.sample(tf.expand_dims(observation, axis=0)).numpy()[0]
+        observation_next, reward, done, _ = self.env.step(action)
+        transition = {'observation': observation, 'observation_next': observation_next,
+                      'action': action, 'reward': reward, 'done': done}
+        if training:
+            self.replay_buffer.store_transition(transition)
+            for m in self.metrics:
+                m.record(transition)
+        return transition
 
-                loss = self._update_policy(
-                    observations=tf.convert_to_tensor(data['observations'], tf.float32),
-                    actions=tf.convert_to_tensor(data['actions'], tf.int32),
-                    total_rewards=tf.convert_to_tensor(data['total_rewards'], tf.float32)
-                )
-
-                e = self.ckpt.epochs_done.numpy()
-                if e % self.ckpt_epochs == 0 or e == self.epochs:
-                    self.ckpt_manager.save()
-                if e % self.log_epochs == 0 or e == self.epochs:
-                    with summary_writer.as_default(), tf.name_scope('training'):
-                        tf.summary.scalar('total_rewards', tf.reduce_mean([tf.reduce_mean(t) for t in tf.split(
-                            data['total_rewards'], data['episode_lengths']
-                        )]), step=self.ckpt.epochs_done)
-                        tf.summary.scalar('episode_lengths', tf.reduce_mean(data['episode_lengths']),
-                                          step=self.ckpt.epochs_done)
-                        tf.summary.scalar('policy_loss', loss, step=self.ckpt.epochs_done)
-
-                self.ckpt.epochs_done.assign_add(1)
-                pbar.update(1)
-            self.env.close()
+    def update(self):
+        dataset = self.replay_buffer.as_dataset(num_batches=1, batch_size=None)
+        data = next(iter(dataset))
+        result = {
+            'policy_loss': self._update_policy(data['observation'], data['action'], data['episode_return']),
+            **{m.name: m.compute() for m in self.metrics},
+        }
+        self.replay_buffer.purge()
+        for metric in self.metrics:
+            metric.reset()
+        return result
 
     @tf.function(experimental_relax_shapes=True)
-    def _update_policy(self, observations, actions, total_rewards):
-        total_rewards -= tf.reduce_mean(total_rewards)
-        total_rewards /= tf.math.reduce_std(total_rewards) + 1e-8
+    def _update_policy(self, observation, action, episode_return):
+        episode_return -= tf.reduce_mean(episode_return)
+        episode_return /= tf.math.reduce_std(episode_return) + 1e-8
         with tf.GradientTape() as tape:
-            log_probs = self.policy.log_prob(observations, actions)
-            loss = -tf.reduce_mean(log_probs * total_rewards)
+            log_probs = self.policy.log_prob(observation, action)
+            loss = -tf.reduce_mean(log_probs * episode_return)
             gradients = tape.gradient(loss, self.policy.trainable_variables)
             self.optimizer.apply_gradients(zip(gradients, self.policy.trainable_variables))
         return loss
--- zoo/cartpole/vpg.py
+++ zoo/cartpole/vpg.py
@@ -1,6 +1,7 @@
 import gym
 
 from rl.agents.vpg import VPG
+from rl.loops import EpisodeTrainLoop
 from zoo.cartpole.core import PolicyNetwork, parse_args, get_output_dirs, evaluate_policy
 
 if __name__ == '__main__':
@@ -11,16 +12,21 @@ if __name__ == '__main__':
         env=gym.make('CartPole-v0'),
         policy_fn=PolicyNetwork,
         lr=1e-3,
-        epochs=1000,
-        episodes_per_epoch=2,
+        replay_buffer_size=250 * 2,
+        metrics_buffer_size=250 * 2,
+    )
+    train_loop = EpisodeTrainLoop(
+        agent=agent,
+        n_episodes=2000,
         max_episode_length=250,
-        ckpt_epochs=10,
-        log_epochs=1,
         ckpt_dir=ckpt_dir,
-        log_dir=log_dir
+        log_dir=log_dir,
+        ckpt_every=100,
+        log_every=10,
+        update_every=2,
     )
 
     if args.mode == 'train':
-        agent.train()
+        train_loop.run()
     if args.mode == 'evaluate':
         evaluate_policy(agent.env, agent.policy)
