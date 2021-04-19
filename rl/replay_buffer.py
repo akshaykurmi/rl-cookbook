a8465876fb24fe48d8067e5148a56ae592e13942
Akshay Kurmi <akskurmi@gmail.com>
2021-04-19 16:05:21 -0400

    Refactor replay buffers

--- rl/replay_buffer.py
+++ rl/replay_buffer.py
@@ -46,10 +46,8 @@ class ReplayBuffer(ABC):
             if not dependencies.issubset(store_field_names):
                 raise ValueError(f'Compute field {f.name} requires store fields {dependencies}')
 
-        self.store_buffers = {f.name: RingBuffer(self.buffer_size, f.shape, f.dtype)
-                              for f in self.store_fields}
-        self.compute_buffers = {f.name: RingBuffer(self.buffer_size, f.shape, f.dtype)
-                                for f in self.compute_fields}
+        self.buffers = {f.name: RingBuffer(self.buffer_size, f.shape, f.dtype)
+                        for f in self.store_fields + self.compute_fields}
         self.current_size, self.compute_head = 0, 0
 
     @abstractmethod
@@ -57,15 +55,15 @@ class ReplayBuffer(ABC):
         self._compute()
 
     def purge(self):
-        for buffer in [*self.store_buffers.values(), *self.compute_buffers.values()]:
+        for buffer in self.buffers.values():
             buffer.purge()
         self.current_size, self.compute_head = 0, 0
 
     def store_transition(self, transition):
         for f in self.store_fields:
-            self.store_buffers[f.name].append(transition[f.name])
+            self.buffers[f.name].append(transition[f.name])
         for f in self.compute_fields:
-            self.compute_buffers[f.name].append(np.zeros(f.shape))
+            self.buffers[f.name].append(np.zeros(f.shape))
         if self.current_size == self.buffer_size:
             self.compute_head = max(self.compute_head - 1, 0)
         self.current_size = min(self.current_size + 1, self.buffer_size)
@@ -75,35 +73,35 @@ class ReplayBuffer(ABC):
             return
 
         indices = np.arange(self.compute_head, self.current_size)
-        tail_indices = 1 + indices[self.store_buffers['done'][self.compute_head:]]
+        tail_indices = 1 + indices[self.buffers['done'][self.compute_head:]]
         # If the last index is not done, add it to the tail_indices
-        if self.store_buffers['done'][-1] is False:
+        if self.buffers['done'][-1] is False:
             tail_indices = np.concatenate((tail_indices, [self.current_size]))
 
         for compute_tail in tail_indices:
             for f in self.compute_fields:
                 self.compute_config[f.name]['func'](self.compute_head, compute_tail)
             # If the last index is not done, do not move the compute_head
-            if compute_tail < self.current_size or self.store_buffers['done'][-1]:
+            if compute_tail < self.current_size or self.buffers['done'][-1]:
                 self.compute_head = compute_tail
 
     def _compute_advantage(self, head, tail):
-        rewards = self.store_buffers['reward'][head:tail]
-        values = self.store_buffers['value'][head:tail]
-        value_nexts = self.store_buffers['value_next'][head:tail]
+        rewards = self.buffers['reward'][head:tail]
+        values = self.buffers['value'][head:tail]
+        value_nexts = self.buffers['value_next'][head:tail]
         deltas = rewards + self.gamma * value_nexts - values
-        self.compute_buffers['advantage'][head:tail] = self._discounted_cumsum(deltas, self.gamma * self.lambda_)
+        self.buffers['advantage'][head:tail] = self._discounted_cumsum(deltas, self.gamma * self.lambda_)
 
     def _compute_reward_to_go(self, head, tail):
-        rewards = self.store_buffers['reward'][head:tail]
-        self.compute_buffers['reward_to_go'][head:tail] = self._discounted_cumsum(rewards, self.gamma)
+        rewards = self.buffers['reward'][head:tail]
+        self.buffers['reward_to_go'][head:tail] = self._discounted_cumsum(rewards, self.gamma)
 
     def _compute_episode_return(self, head, tail):
-        episode_return = np.sum(self.store_buffers['reward'][head:tail])
-        self.compute_buffers['episode_return'][head:tail] = episode_return
+        episode_return = np.sum(self.buffers['reward'][head:tail])
+        self.buffers['episode_return'][head:tail] = episode_return
 
     def _compute_episode_length(self, head, tail):
-        self.compute_buffers['episode_length'][head:tail] = tail - head + 1
+        self.buffers['episode_length'][head:tail] = tail - head + 1
 
     @staticmethod
     def _discounted_cumsum(values, discount):
@@ -117,13 +115,12 @@ class ReplayBuffer(ABC):
         return scipy.signal.lfilter([1], [1, float(-discount)], values[::-1], axis=0)[::-1]
 
 
-class DeterministicReplayBuffer(ReplayBuffer):
-    def as_dataset(self, num_batches=1, batch_size=None):
+class OnePassReplayBuffer(ReplayBuffer):
+    def as_dataset(self, batch_size=32):
         def data_generator():
-            for i in range(self.current_size):
-                store_data = {k: buf[i] for k, buf in self.store_buffers.items()}
-                compute_data = {k: buf[i] for k, buf in self.compute_buffers.items()}
-                yield {**store_data, **compute_data}
+            for i in np.random.default_rng().choice(self.current_size, size=self.current_size, replace=False):
+                yield {f.name: self.buffers[f.name][i.item()]
+                       for f in self.store_fields + self.compute_fields}
 
         super().as_dataset()
         dataset = tf.data.Dataset.from_generator(
@@ -131,20 +128,16 @@ class DeterministicReplayBuffer(ReplayBuffer):
             output_types={f.name: tf.as_dtype(f.dtype) for f in self.store_fields + self.compute_fields},
             output_shapes={f.name: f.shape for f in self.store_fields + self.compute_fields}
         )
-        dataset = dataset.repeat(-1)
-        dataset = dataset.batch(batch_size or self.current_size)
-        dataset = dataset.take(num_batches)
+        dataset = dataset.batch(batch_size)
         dataset = dataset.prefetch(tf.data.AUTOTUNE)
         return dataset
 
 
 class UniformReplayBuffer(ReplayBuffer):
-    def as_dataset(self, num_batches=1, batch_size=None):
+    def as_dataset(self, batch_size=32):
         def data_generator():
             i = np.random.randint(self.current_size)
-            store_data = {k: buf[i] for k, buf in self.store_buffers.items()}
-            compute_data = {k: buf[i] for k, buf in self.compute_buffers.items()}
-            yield {**store_data, **compute_data}
+            yield {k: buf[i] for k, buf in self.buffers.items()}
 
         super().as_dataset()
         dataset = tf.data.Dataset.from_generator(
@@ -153,7 +146,6 @@ class UniformReplayBuffer(ReplayBuffer):
             output_shapes={f.name: f.shape for f in self.store_fields + self.compute_fields}
         )
         dataset = dataset.repeat(-1)
-        dataset = dataset.batch(batch_size or self.current_size)
-        dataset = dataset.take(num_batches)
+        dataset = dataset.batch(batch_size)
         dataset = dataset.prefetch(tf.data.AUTOTUNE)
         return dataset
