20caf05c0f62c2f4092f0f7aa737a6e64836e9d4
Akshay Kurmi <akskurmi@gmail.com>
2021-04-27 21:04:36 -0400

    Refactor SAC to use updated train loops and replay buffer

--- rl/agents/sac.py
+++ rl/agents/sac.py
@@ -1,15 +1,13 @@
+import numpy as np
 import tensorflow as tf
-from tqdm import tqdm
 
-from rl.metrics import AverageEpisodeLength
-from rl.metrics import AverageReturn
-from rl.replay_buffer import UniformReplayBuffer
+from rl.replay_buffer import UniformReplayBuffer, ReplayField
+from rl.utils import MeanAccumulator
 
 
 class SAC:
-    def __init__(self, env, policy_fn, qf_fn, lr_policy, lr_qf, gamma, polyak, alpha, episodes, max_episode_length,
-                 replay_buffer_size, initial_random_episodes, update_every_steps, update_iterations, update_batch_size,
-                 ckpt_episodes, log_episodes, ckpt_dir, log_dir):
+    def __init__(self, env, policy_fn, qf_fn, lr_policy, lr_qf, gamma, polyak, alpha,
+                 update_iterations, update_batch_size, replay_buffer_size):
         self.env = env
         self.policy = policy_fn()
         self.qf1 = qf_fn()
@@ -25,95 +23,58 @@ class SAC:
         self.gamma = gamma
         self.polyak = polyak
         self.alpha = alpha
-        self.episodes = episodes
-        self.max_episode_length = max_episode_length
-        self.replay_buffer_size = replay_buffer_size
-        self.initial_random_episodes = initial_random_episodes
-        self.update_every_steps = update_every_steps
         self.update_iterations = update_iterations
         self.update_batch_size = update_batch_size
-        self.ckpt_episodes = ckpt_episodes
-        self.log_episodes = log_episodes
-        self.ckpt_dir = ckpt_dir
-        self.log_dir = log_dir
 
         self.replay_buffer = UniformReplayBuffer(
-            buffer_size=self.replay_buffer_size,
-            store_fields=['observation', 'action', 'reward', 'observation_next', 'done'],
-            compute_fields=[]
+            buffer_size=replay_buffer_size,
+            store_fields=[
+                ReplayField('observation', shape=self.env.observation_space.shape,
+                            dtype=self.env.observation_space.dtype),
+                ReplayField('observation_next', shape=self.env.observation_space.shape,
+                            dtype=self.env.observation_space.dtype),
+                ReplayField('action', shape=self.env.action_space.shape,
+                            dtype=self.env.action_space.dtype),
+                ReplayField('reward'),
+                ReplayField('done', dtype=np.bool),
+            ],
+            compute_fields=[],
         )
-        self.metrics = [
-            AverageReturn(self.log_episodes * self.max_episode_length),
-            AverageEpisodeLength(self.log_episodes * self.max_episode_length)
-        ]
-        self.episodes_done = tf.Variable(0, dtype=tf.int64, trainable=False)
         self.policy_optimizer = tf.keras.optimizers.Adam(learning_rate=self.lr_policy)
         self.qf_optimizer = tf.keras.optimizers.Adam(learning_rate=self.lr_qf)
-        self.ckpt = tf.train.Checkpoint(policy=self.policy, qf1=self.qf1, qf2=self.qf2,
-                                        policy_target=self.policy_target,
-                                        qf1_target=self.qf1_target, qf2_target=self.qf2_target,
-                                        policy_optimizer=self.policy_optimizer, qf_optimizer=self.qf_optimizer,
-                                        episodes_done=self.episodes_done)
-        self.ckpt_manager = tf.train.CheckpointManager(self.ckpt, self.ckpt_dir, max_to_keep=1)
-        self.ckpt.restore(self.ckpt_manager.latest_checkpoint).expect_partial()
 
-    def train(self):
-        summary_writer = tf.summary.create_file_writer(self.log_dir)
-        with tqdm(total=self.episodes, desc='Training', unit='episode') as pbar:
-            pbar.update(self.ckpt.episodes_done.numpy())
-            global_step = 0
-            while self.ckpt.episodes_done.numpy() <= self.episodes:
-                observation = self.env.reset()
-                for step in range(self.max_episode_length):
-                    global_step += 1
-                    if self.ckpt.episodes_done.numpy() <= self.initial_random_episodes:
-                        action = self.env.action_space.sample()
-                    else:
-                        action = self.policy.sample(observation.reshape(1, -1), return_entropy=False).numpy()[0]
-                    observation_next, reward, done, _ = self.env.step(action)
-                    transition = {'observation': observation, 'action': action, 'reward': reward,
-                                  'observation_next': observation_next, 'done': done}
-                    observation = observation_next
-                    self.replay_buffer.store_transition(transition)
-                    for m in self.metrics:
-                        m.record(transition)
-                    if global_step % self.update_every_steps == 0:
-                        policy_loss, qf_loss = self._update()
-                    if done:
-                        break
+    def variables_to_checkpoint(self):
+        return {'policy': self.policy, 'qf1': self.qf1, 'qf2': self.qf2,
+                'policy_target': self.policy_target, 'qf1_target': self.qf1_target, 'qf2_target': self.qf2_target,
+                'policy_optimizer': self.policy_optimizer, 'qf_optimizer': self.qf_optimizer}
 
-                e = self.ckpt.episodes_done.numpy()
-                if e % self.ckpt_episodes == 0 or e == self.episodes:
-                    self.ckpt_manager.save()
-                if e % self.log_episodes == 0 or e == self.episodes:
-                    with summary_writer.as_default(), tf.name_scope('training'):
-                        for m in self.metrics:
-                            tf.summary.scalar(m.name, m.compute(), step=self.ckpt.episodes_done)
-                        tf.summary.scalar('policy_loss', policy_loss, step=self.ckpt.episodes_done)
-                        tf.summary.scalar('qf_loss', qf_loss, step=self.ckpt.episodes_done)
+    def step(self, previous_transition=None, training=False, random_action=False):
+        observation = previous_transition['observation_next'] if previous_transition else self.env.reset()
+        action = self.env.action_space.sample() if random_action else \
+            self.policy.sample(observation.reshape(1, -1), return_entropy=False).numpy()[0]
+        observation_next, reward, done, _ = self.env.step(action)
+        transition = {'observation': observation, 'observation_next': observation_next,
+                      'action': action, 'reward': reward, 'done': done}
+        if training:
+            self.replay_buffer.store_transition(transition)
+        return transition
 
-                for m in self.metrics:
-                    m.reset()
-                self.ckpt.episodes_done.assign_add(1)
-                pbar.update(1)
-            self.env.close()
-
-    def _update(self):
-        policy_losses, qf_losses = [], []
-        for i in range(self.update_iterations):
-            batch = self.replay_buffer.sample_batch(self.update_batch_size)
-            observation = tf.convert_to_tensor(batch['observation'], tf.float32)
-            action = tf.convert_to_tensor(batch['action'], tf.float32)
-            reward = tf.convert_to_tensor(batch['reward'], tf.float32)
-            observation_next = tf.convert_to_tensor(batch['observation_next'], tf.float32)
-            done = tf.convert_to_tensor(batch['done'], tf.float32)
-            qf_losses.append(self._update_qf(observation, action, reward, observation_next, done))
-            policy_losses.append(self._update_policy(observation))
+    def update(self):
+        dataset = self.replay_buffer.as_dataset(self.update_batch_size).take(self.update_iterations)
+        policy_loss_acc, qf_loss_acc = MeanAccumulator(), MeanAccumulator()
+        for data in dataset:
+            qf_loss_acc.add(self._update_qf(data))
+            policy_loss_acc.add(self._update_policy(data))
             self._update_targets()
-        return tf.reduce_mean(policy_losses), tf.reduce_mean(qf_losses)
+        return {
+            'policy_loss': policy_loss_acc.value(),
+            'qf_loss': qf_loss_acc.value(),
+        }
 
     @tf.function(experimental_relax_shapes=True)
-    def _update_qf(self, observation, action, reward, observation_next, done):
+    def _update_qf(self, data):
+        observation, observation_next = data['observation'], data['observation_next']
+        action, reward, done = data['action'], data['reward'], data['done']
         with tf.GradientTape(watch_accessed_variables=False) as tape:
             tape.watch(self.qf1.trainable_variables)
             tape.watch(self.qf2.trainable_variables)
@@ -133,7 +94,8 @@ class SAC:
         return loss
 
     @tf.function(experimental_relax_shapes=True)
-    def _update_policy(self, observation):
+    def _update_policy(self, data):
+        observation = data['observation']
         with tf.GradientTape(watch_accessed_variables=False) as tape:
             tape.watch(self.policy.trainable_variables)
             a, e = self.policy.sample(observation)
