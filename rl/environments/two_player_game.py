d1e64c38fd685695b1a7e81e3a82294b83703321
Akshay Kurmi <akskurmi@gmail.com>
2021-05-04 16:25:29 -0400

    Add abstract class for two player game

---
+++ rl/environments/two_player_game.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+
+class TwoPlayerGame(ABC):
+    # Set these in subclasses
+    metadata = {'render.modes': []}
+    action_space = None
+    observation_space = None
+
+    @abstractmethod
+    def step(self, action, canonical=True):
+        raise NotImplementedError
+
+    @abstractmethod
+    def reset(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def render(self, mode='human'):
+        raise NotImplementedError
+
+    @abstractmethod
+    def valid_actions(self, canonical=True):
+        raise NotImplementedError
+
+    @abstractmethod
+    def observation(self, canonical=True):
+        raise NotImplementedError
+
+    @abstractmethod
+    def score(self, canonical=True):
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_over(self):
+        raise NotImplementedError
+
+    def close(self):
+        pass
+
+    def __str__(self):
+        return f'<{type(self).__name__} instance>'
